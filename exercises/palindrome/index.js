913f6daf5f8a82d57c276ff0a3b964697fe1d694
mameMan <jokingbear01@gmail.com>
2022-02-21 15:01:48 +0900

    section 4-5. palindromes, integer_reversal

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,36 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  return str.split("").every((char, i) => char === str[str.length - i - 1]);
+}
+console.log(palindrome("abba"));
 
 module.exports = palindrome;
+
+/* solution 1. my solution
+function palindrome(str) {
+  const reversed = str.split("").reduce((rev, char) => char + rev, "");
+  if (reversed !== str) {
+    return false;
+  }
+  return true;
+}
+console.log(palindrome("abba"));
+ */
+
+/* solution 2. return boolean directly
+function palindrome(str) {
+  const reversed = str.split("").reverse().join("");
+
+  return str === reversed;
+}
+console.log(palindrome("abba"));
+ */
+
+/* solution 3. with every()
+function palindrome(str) {
+  return str.split("").every((char, i) => char === str[str.length - i - 1]);
+}
+console.log(palindrome("abba"));
+*/
