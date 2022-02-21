df1e78990302c279ea285a72101ddd961bca7686
mameMan <jokingbear01@gmail.com>
2022-02-22 08:08:40 +0900

    section 7. fizzBuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,43 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; ++i) {
+    // Is the number a multiple of 3 and 5?
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      // Is the number a multiple of 3?
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      // Is the number a multiple of 5?
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+fizzBuzz(15);
 
 module.exports = fizzBuzz;
+
+/* solution 1. my solution
+function fizzBuzz(n) {
+  let arr = [];
+  for (let i = 1; i <= n; ++i) {
+    arr.push(i);
+  }
+
+  for (j = 0; j < arr.length; ++j) {
+    if (arr[j] % 3 === 0 && arr[j] % 5 === 0) {
+      arr[j] = "fizzbuzz";
+    } else if (arr[j] % 5 === 0) {
+      arr[j] = "buzz";
+    } else if (arr[j] % 3 === 0) {
+      arr[j] = "fizz";
+    }
+    console.log(arr[j]);
+  }
+}
+fizzBuzz(15);
+ */
