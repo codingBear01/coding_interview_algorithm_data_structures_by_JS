a77528d8eaec0bafab70edee3ba549bb5eb8d094
mameMan <jokingbear01@gmail.com>
2022-02-24 11:52:19 +0900

    section 17. the_queue

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,24 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+
+  add(record) {
+    this.data.unshift(record);
+  }
+
+  remove() {
+    return this.data.pop();
+  }
+}
+
+const q = new Queue();
+q.add(1);
+console.log(q);
+console.log(q.remove());
+console.log(q);
 
 module.exports = Queue;
